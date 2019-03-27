902419484fc0cbc934cf36ccaadf7499530bdd9c
raithwind <44484397+raithwind@users.noreply.github.com>
2019-03-27 15:34:32 +0000

    Add files via upload

--- getspellbook.js
+++ getspellbook.js
@@ -1,34 +1,43 @@
 function setup() {
-	
+	wlSlider = createSlider(1,20,1);
+	wlSlider.position(20,20)
 }
 
 Array.prototype.sample = function(){
 	return this[Math.floor(Math.random()*this.length)];
 }
 
-function draw() {
-}
 
 function get_max_spell_level(wizlevel){
-	return Math.min(9,Math.floor((wizlevel+1)/2))
+	return Math.min(9,Math.floor((wizlevel+1)/2));
 	}
 
 
 function prod(){
-  console.log(get_spells(5,5));
-  var i;
-  for (i = 1; i < 10; i++){
-	document.getElementById("L"+ i +"_Title").innerHTML = "Level "+i+" Spells";
-    document.getElementById("L"+ i).innerHTML = "List of spells goes here of level "+ i;
+    const wl = wlSlider.value();
+    
+    max_spell = get_max_spell_level(wl)
+    var i;
+    for (i = 1; i < max_spell +1; i++){ 
+            spells_chosen = get_spells(i,5)
+            document.getElementById("L"+ i +"_Title").innerHTML = "Level "+i+" Spells";
+            document.getElementById("L"+ i).innerHTML = spells_chosen;
+    
+  
+
    }  
 }
 
-function get_spells(num,level){
+function get_spells(level,num){
 	var spells=[];
 	for (i = 1; i < num +1; i++){
-	//ch = spell_list[level]
-	choice = spell_list[level].sample();
-		spells.push(choice);
-		}
+        choice = spell_list[level].sample();
+        if (spells.includes(choice)){
+                    
+        }
+        else{
+        spells.push(choice);
+      }
+    }
 	return spells
-	}
+}
