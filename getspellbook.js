31288f9e85658e056ce2655ff916092ee2466a72
raithwind <44484397+raithwind@users.noreply.github.com>
2019-03-30 10:39:39 +0000

    Add files via upload

--- getspellbook.js
+++ getspellbook.js
@@ -1,6 +1,12 @@
 function setup() {
 	wlSlider = createSlider(1,20,1);
-	wlSlider.position(20,20)
+	wlSlider.parent("test")
+	wlSlider.position(20,20);
+	button = createButton('Generate Level '+ wlSlider.elt.value+' book.');
+	button.parent("test")
+	button.position(wlSlider.x + wlSlider.width + 20);
+	button.mousePressed(prod);
+	
 }
 
 Array.prototype.sample = function(){
@@ -8,18 +14,40 @@ Array.prototype.sample = function(){
 }
 
 
-function get_max_spell_level(wizlevel){
-	return Math.min(9,Math.floor((wizlevel+1)/2));
+function get_spell_levels(wizlevel){
+	max_level = Math.min(9,Math.floor((wizlevel+1)/2));
+	var spell_levels = {}
+	for (i = 1; i < max_level + 1; i++){
+		numspells = (1/(i/wizlevel))/1.1;
+		spell_levels[i] = Math.round(numspells);
+		if (i == 1 ){
+			numspells += 6
+		}
+		spell_levels[i] = Math.round(numspells);
+		spell_levels.length = i
+	}
+return spell_levels
 	}
 
-
+function draw(){
+	button.elt.innerHTML = 'Generate Level '+ wlSlider.elt.value+' book.'
+}	
+	
+	
 function prod(){
-    const wl = wlSlider.value();
+    for (i = 1; i < 9 +1; i++){ 
+            document.getElementById("L"+ i +"_Title").innerHTML = " ";
+            document.getElementById("L"+ i).innerHTML = " ";
     
-    max_spell = get_max_spell_level(wl)
+   }  
+	
+	const wl = wlSlider.value();
+	spells_by_level = get_spell_levels(wl)
+	console.log(spells_by_level[1])
+	let max_spell = get_spell_levels(wl).length
     var i;
     for (i = 1; i < max_spell +1; i++){ 
-            spells_chosen = get_spells(i,5)
+            spells_chosen = get_spells(i,spells_by_level[i])
             document.getElementById("L"+ i +"_Title").innerHTML = "Level "+i+" Spells";
             document.getElementById("L"+ i).innerHTML = spells_chosen;
     
